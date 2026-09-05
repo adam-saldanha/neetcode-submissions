eb42786a482b2ab5ad0a096d42afe985a9c53334
adam-saldanha <adamsaldanha@outlook.com>
2026-09-05 16:37:11 -0400

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        hash_s = {}
+        hash_t = {}
+
+        for i in range(len(s)):
+            if s[i] not in hash_s:
+                hash_s[s[i]] = 1
+            else:
+                hash_s[s[i]] += 1
+
+            if t[i] not in hash_t:
+                hash_t[t[i]] = 1
+            else:
+                hash_t[t[i]] += 1
+        
+        for char in s:
+            if char not in t:
+                return False
+            if hash_s[char] != hash_t[char]:
+                return False
+        return True
+
+
+        
